0e63bc6387fd98cae6ab4b9078a51c1f6d6f3443
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-02-13 14:39:55 -0500

    version bump to rebuild CI/CD

--- fastsurfer_inference.py
+++ fastsurfer_inference.py
@@ -24,7 +24,7 @@ logger.remove()
 logger.opt(colors = True)
 logger.add(sys.stderr, format=logger_format)
 
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 DISPLAY_TITLE = r"""
   __          _                   __          _        __                             
